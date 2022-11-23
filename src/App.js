77972b17daf9de180f94bd40ecfa5f0ce37ea933
Henrique Portela <henriquepsoares@gmail.com>
2022-11-23 00:22:52 -0300

    First Part

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { Card, Row, Col, Divider, Input, Button } from 'antd';
+import foodList from './foods.json';
+import { useState } from 'react'
+import FoodBox from './Components/FoodBox';
 
-function App() {
+function App(props) {
+  const [foods, setFoods] = useState(foodList)
+    
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+      {foods.map((food) => {
+        return (
+
+      <FoodBox food={food} />
+        )
+      })}
+      </>
+
+
+
     </div>
   );
 }
